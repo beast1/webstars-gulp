c65fda1e5a29e8e781b07594dcd78d313c71e7a9
Egor <beast.loby@gmail.com>
2017-07-13 21:58:53 +0300

    replace native require to getTask()

--- mts/gulpfile.js
+++ mts/gulpfile.js
@@ -19,16 +19,24 @@ var s = {
 	"build": build
 }
 
-function getSubTask(task) {
+function getTask(task) {
   return require('../global/gulp-tasks/' + task)(gulp, p, s);
 }
 
-gulp.task('sass', require('../global/gulp-tasks/sass')(gulp, p, s));
-gulp.task('zip', require('../global/gulp-tasks/zip')(gulp, p, s));
-gulp.task('serve', require('../global/gulp-tasks/serve')(gulp, p, s));
-gulp.task('watch', ['sass', 'html', 'serve'], require('../global/gulp-tasks/watch')(gulp, p, s));
-gulp.task('html', require('../global/gulp-tasks/html')(gulp, p, s));
-// gulp.task('build', ['sass', 'html'], require('../global/gulp-tasks/build')(gulp, p, s));
-gulp.task('build', ['sass', 'html'], getSubTask('build'));
+gulp.task('sass', getTask('sass'));
+gulp.task('zip', getTask('zip'));
+gulp.task('serve', getTask('serve'));
+gulp.task('watch', ['sass', 'html', 'serve'], getTask('watch'));
+gulp.task('html', getTask('html'));
+gulp.task('build', ['sass', 'html'], getTask('build'));
 
 gulp.task('default', ['watch']);
+
+// gulp.task('sass', require('../global/gulp-tasks/sass')(gulp, p, s));
+// gulp.task('zip', require('../global/gulp-tasks/zip')(gulp, p, s));
+// gulp.task('serve', require('../global/gulp-tasks/serve')(gulp, p, s));
+// gulp.task('watch', ['sass', 'html', 'serve'], require('../global/gulp-tasks/watch')(gulp, p, s));
+// gulp.task('html', require('../global/gulp-tasks/html')(gulp, p, s));
+// gulp.task('build', ['sass', 'html'], require('../global/gulp-tasks/build')(gulp, p, s));
+
+
