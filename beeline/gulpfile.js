ac8c749463e8c4247a585896875b40fa0e425e45
Egor <beast.loby@gmail.com>
2017-07-14 02:25:33 +0300

    init bee

---
+++ beeline/gulpfile.js
@@ -0,0 +1,50 @@
+// Как выполнять таски синхронно?
+// Как использовать значения, введенные в консоль, в теле таска
+
+var gulp = require('gulp');
+var p = require('gulp-load-plugins')({
+	pattern: ['browser-sync', 'gulp-*', 'gulp.*', '@*/gulp{-,.}*'],
+	lazy: false
+});
+
+var	oss            = "megafon",
+		globalConfig   = require('../global/config.json').global,
+		localConfig 	 = require('../global/config.json').megafon,
+		defaultConfig  = require('../global/config.json').default,
+		project 			 = localConfig.projects[localConfig.exec],
+		app 					 = project.name,
+		build  				 = defaultConfig.dirs.build;
+
+var s = {
+	"oss": oss,
+	"globalConfig": globalConfig,
+	"localConfig": localConfig,
+	"defaultConfig": defaultConfig,
+	"project": project,
+	"app": app,
+	"build": build
+}
+
+function getTask(task) {
+  return require('../global/gulp-tasks/' + task)(gulp, p, s);
+}
+
+gulp.task('sass', getTask('sass'));
+gulp.task('zip', getTask('zip'));
+gulp.task('serve', getTask('serve'));
+gulp.task('watch', ['sass', 'html', 'serve'], getTask('watch'));
+gulp.task('html', getTask('html'));
+gulp.task('build', ['sass', 'html'], getTask('build'));
+
+gulp.task('default', ['watch']);
+
+// gulp.task('sass', require('../global/gulp-tasks/sass')(gulp, p, s));
+// gulp.task('zip', require('../global/gulp-tasks/zip')(gulp, p, s));
+// gulp.task('serve', require('../global/gulp-tasks/serve')(gulp, p, s));
+// gulp.task('watch', ['sass', 'html', 'serve'], require('../global/gulp-tasks/watch')(gulp, p, s));
+// gulp.task('html', require('../global/gulp-tasks/html')(gulp, p, s));
+// gulp.task('build', ['sass', 'html'], require('../global/gulp-tasks/build')(gulp, p, s));
+
+gulp.task('clearcache', function () { return cache.clearAll(); });
+
+gulp.task('default', ['watch']);
--- global/gulp-tasks/zip.js
+++ global/gulp-tasks/zip.js
@@ -1,3 +1,5 @@
+"use strict";
+
 module.exports = function (gulp, p, s) {
 	return function() {
 		var getVersion = function() {
@@ -8,11 +10,10 @@ module.exports = function (gulp, p, s) {
 
 		var buildArchiv = gulp.src(s.build + '/**/*.*')
 			.pipe(p.zip(s.oss + '-' + s.app + '.v' + getVersion() + '.zip'))
-			.pipe(gulp.dest(s.globalConfig.dirs.release))
 			.pipe(gulp.dest(s.defaultConfig.dirs.history))
 			.pipe(gulp.dest(s.globalConfig.dirs.history));
 
-		console.log('---------- Релиз ' + s.oss + '-' + s.app + '.v' + getVersion() + '.zip добавлен в config.json и историю ');
+		console.log(`---------- Релиз ${s.oss}-${s.app}.v${getVersion()}.zip добавлен в config.json и историю.\n---------- Доступен в ${s.globalConfig.dirs.history}`);
 
 		if (s.oss === 'mts') {
 			return gulp.src("../global/config.json")
