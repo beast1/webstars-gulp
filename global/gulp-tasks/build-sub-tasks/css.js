01800c05ff24e6a9771a603985334993ca7af7da
Egor <beast.loby@gmail.com>
2017-07-14 19:24:35 +0300

    upd megafon data.json

--- global/gulp-tasks/build-sub-tasks/css.js
+++ global/gulp-tasks/build-sub-tasks/css.js
@@ -1,19 +1,18 @@
 module.exports = function (gulp, p, s) {
-		switch (s.oss) {
-			case "mts": 
-				var buildCss = gulp.src([
-						s.app + '/css/style.css',
-						s.app + '/css/normalize.css'
-					])
-					.pipe(gulp.dest(s.build + '/css'));
-			break
-			case "megafon": 
-				var buildCss = gulp.src([
-						s.app + '/css/style.css',
-						s.app + '/css/normalize.css'
-					])
-					.pipe(p.replace('../img/', ''))
-					.pipe(gulp.dest(s.build));
-			break
-		}
+	if (s.oss === 'mts') {
+		var buildCss = gulp.src([
+				s.app + '/css/style.css',
+				s.app + '/css/normalize.css'
+			])
+			.pipe(gulp.dest(s.build + '/css'));
+	} else if (s.oss === 'megafon') {
+		console.log(`---------- Соберем весь css в один файл`);
+		var buildCss = gulp.src([
+				s.app + '/css/style.css',
+				s.app + '/css/normalize.css'
+			])
+			.pipe(p.replace('../img/', ''))
+			.pipe(p.concat('style.css'))
+			.pipe(gulp.dest(s.build));
+	}
 }
