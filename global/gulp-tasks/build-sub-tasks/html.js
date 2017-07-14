1eb002a49ea50b7d2a3864312ef0a457abd74a42
Egor <beast.loby@gmail.com>
2017-07-14 17:48:34 +0300

    refactoring build-sub-modules/html

--- global/gulp-tasks/build-sub-tasks/html.js
+++ global/gulp-tasks/build-sub-tasks/html.js
@@ -1,47 +1,34 @@
 module.exports = function (gulp, p, s) {
-		if (s.oss === 'mts') {
-				var buildHtml = gulp.src([
-						s.app + '/full-page.html',
-						s.app + '/html/blocks/*.html'
-					])
-					.pipe(gulp.dest(s.build));
+	if (s.oss === 'mts') {
+			var buildHtml = gulp.src([
+					s.app + '/full-page.html',
+					s.app + '/html/blocks/*.html'
+				])
+				.pipe(gulp.dest(s.build));
 
-				if (s.project.wap === false) {
-					var copyHtml = gulp.src([
-						s.build + '/header.html',
-						s.build + '/footer.html'
-					])
-					.pipe(p.rename({
-						suffix: '.phone'
-					}))
-					.pipe(gulp.dest(s.build));
-				}
-			} else if (s.oss === 'megafon') {
-
-				var buildHtml = gulp.src(s.app + '/sign-in.html')
-					.pipe(p.replace('css/', ''))
-					.pipe(p.replace('img/', ''))
-					.pipe(p.rename({
-						basename: 'страница авторизации'
-					}))
-					.pipe(gulp.dest(s.build));
-
-				var buildHtml2 = gulp.src(s.app + '/subscribe.html')
-					.pipe(p.replace('css/', ''))
-					.pipe(p.replace('img/', ''))
-					.pipe(p.rename({
-						basename: 'страница подписки'
-					}))
-					.pipe(gulp.dest(s.build));
-
-				var buildHtml3 = gulp.src(s.app + '/describe.html')
-					.pipe(p.replace('css/', ''))
-					.pipe(p.replace('img/', ''))
-					.pipe(p.rename({
-						basename: 'страница отписки'
-					}))
-					.pipe(gulp.dest(s.build));
+			if (s.project.wap === false) {
+				var copyHtml = gulp.src([
+					s.build + '/header.html',
+					s.build + '/footer.html'
+				])
+				.pipe(p.rename({
+					suffix: '.phone'
+				}))
+				.pipe(gulp.dest(s.build));
 			}
+	} else if (s.oss === 'megafon') {
+		console.log(`---------- Корректируем пути к файлам и изображениям`);
+		var data = require('D:/webstars/megafon/const/data.json');
+		for (let i = 0; i < data.length; i++) {
+			var buildHtml = gulp.src(`${s.app} + '/${data[i].page}.html'`)
+				.pipe(p.replace('href="css/', 'href="'))
+				.pipe(p.replace('img/', ''))
+				.pipe(p.rename({
+					basename: `${data[i].name}`
+				}))
+				.pipe(gulp.dest(s.build));
+		}
+	}
 }
 
 // https://stackoverflow.com/questions/37043095/gulp-rename-multiple-files
