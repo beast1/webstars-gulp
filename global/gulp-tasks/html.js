bd846e8c0cdfcc37948897cfa02e39db2ecc039c
Egor <beast.loby@gmail.com>
2017-07-14 17:13:57 +0300

    add replace system

--- global/gulp-tasks/html.js
+++ global/gulp-tasks/html.js
@@ -1,6 +1,5 @@
-//Подключить json
-//Прописать файл-инклуд в хтмл-файлах
-//Сделать реплейс футера при компиляции
+"use strict";
+
 module.exports = function (gulp, p, s) {
 	if (s.oss === 'mts') {
 		return function() {
@@ -13,19 +12,37 @@ module.exports = function (gulp, p, s) {
 		    .pipe(p.browserSync.reload({stream: true}));
 		};
 	} else if (s.oss === 'megafon') {
-		// return function() {
-		// 	var data = require('D:/webstars/megafon/const/data.json');
-		// 	return gulp.src([
-		// 			s.app + '/html/sign-in.html',
-		// 			s.app + '/html/subscribe.html',
-		// 			s.app + '/html/describe.html'
-		// 		])
-		// 		.pipe(p.fileInclude({
-		// 	     prefix: '@@',
-		// 	     basepath: '@file'
-		// 	   }))
-		// 	   .pipe(gulp.dest(s.app))
-		// 	   .pipe(p.browserSync.reload({stream: true}));
-		// };
+			console.log(`---------- Сборка блоков из D:/webstars/megafon/${s.app}/html/blocks`);
+			console.log(`---------- Подстановка значений из D:/webstars/megafon/const/data.json`);
+			var data = require('D:/webstars/megafon/const/data.json');
+
+			for (let i = 0; i < data.length; i++) {
+				let html = gulp.src(`${s.app}/html/${data[i].page}.html`)
+				.pipe(p.fileInclude({
+			     prefix: '@@',
+			     basepath: '@file'
+			   }))
+				 .pipe(p.replace(`%title`, `${data[i].title}`))
+				 .pipe(p.replace(`%footer`, `${data[i].footer}`))
+				 .pipe(p.replace(`%p.text`, `${data[i].inputs.phone.label}`))
+				 .pipe(p.replace(`%p.id`, `${data[i].inputs.phone.id}`))
+				 .pipe(p.replace(`%p.class`, `${data[i].inputs.phone.class}`))
+
+				 .pipe(p.replace(`%c.text`, `${data[i].inputs.captcha.label}`))
+				 .pipe(p.replace(`%c.id`, `${data[i].inputs.captcha.id}`))
+				 .pipe(p.replace(`%c.class`, `${data[i].inputs.captcha.class}`))
+				 .pipe(p.replace(`%c.src`, `${data[i].inputs.captcha.src}`))
+				 .pipe(p.replace(`%c.reload`, `${data[i].inputs.captcha.reload}`))
+
+				 .pipe(p.replace(`%s.text`, `${data[i].inputs.sms.label}`))
+				 .pipe(p.replace(`%s.id`, `${data[i].inputs.sms.id}`))
+				 .pipe(p.replace(`%s.class`, `${data[i].inputs.sms.class}`))
+
+				 .pipe(p.replace(`%b.submit`, `${data[i].buttons.submit}`))
+				 .pipe(p.replace(`%b.cencel`, `${data[i].buttons.cencel}`))
+				 .pipe(p.htmlBeautify())
+			   .pipe(gulp.dest(s.app))
+			   .pipe(p.browserSync.reload({stream: true}));
+			}
 	};
 };
