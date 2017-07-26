c47912911c4a550209e7f25218bc6ec45acca963
Egor <beast.loby@gmail.com>
2017-07-26 13:52:38 +0300

    upd start-packs/tele2

--- global/new.js
+++ global/new.js
@@ -8,8 +8,6 @@ var schema = {
   		require: true
   	},
     name: {
-      pattern: /^[a-zA-Z\s\-]+$/,
-      message: 'Name must be only letters, spaces, or dashes',
       required: true
     }
   }
@@ -20,13 +18,12 @@ prompt.start();
 prompt.get(schema, function (err, result) {
 	if (result.oss === 'mts') {
 		var file = 'config.json';
-		myJson = require('./config.json');
-		var obj = JSON.parse(myJson);
+		var obj = require('./config.json');
 		obj.mts.projects.push({"name": result.name});
 
 		console.log(obj);
 		 
-		json.writeFileSync(file, obj, {flag: 'a'}, function (err) {
+		json.writeFile(file, obj, {flag: 'a'}, function (err) {
 		  console.error(err);
 		});
 	} else {
